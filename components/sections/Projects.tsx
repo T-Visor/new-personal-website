0cf65cb89a94cd1dd6814987ac87fce24e7a96af
T-Visor <turkimbro@yahoo.com>
2025-07-26 00:52:19 -0400

    refactored projects section

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { GithubIcon } from "lucide-react";
 
@@ -28,15 +27,19 @@ const projectData = [
     github: "https://github.com/T-Visor/decruehd-framework",
     publication: "https://ieeexplore.ieee.org/document/9798233",
   },
-  // Add more project objects here...
 ];
 
 export const Projects = () => {
   return (
     <section
       id="projects"
-      className="min-h-screen flex flex-col justify-center items-center px-5 dark:bg-gray-900 pb-20"
+      className="
+        min-h-screen flex flex-col justify-center items-center 
+        pb-20 px-5 
+        dark:bg-gray-900
+      "
     >
+      {/* Section title */}
       <div className="text-4xl font-semibold mb-10 pt-8">
         <h2>Projects</h2>
       </div>
@@ -45,15 +48,29 @@ export const Projects = () => {
         {projectData.map((project, index) => (
           <div
             key={index}
-            className={`flex flex-col md:flex-row ${index % 2 !== 0 ? "md:flex-row-reverse" : ""
-              } items-center gap-10 border-t dark:border-gray-600 pt-10 sm:border-0 sm:pb-0`}
+            className={`
+              flex flex-col md:flex-row items-center 
+              pt-10 gap-10 border-t sm:border-0 sm:pb-0
+              ${index % 2 !== 0 ? "md:flex-row-reverse" : ""} 
+              dark:border-gray-600 
+            `}
           >
-            {/* Text Section */}
-            <div className="flex-1 text-gray-600 dark:text-gray-300 text-center md:text-left">
-              <h2 className="text-3xl font-semibold py-3 text-gray-700 dark:text-gray-200">
+            <div className="
+              flex-1 text-center md:text-left
+              text-gray-600 dark:text-gray-300 
+            ">
+              {/* Project title */}
+              <h2 className="
+                text-3xl font-semibold py-3 
+                text-gray-700 dark:text-gray-200
+              ">
                 {project.title}
               </h2>
+
+              {/* Project description */}
               <p className="text-xl py-3">{project.description}</p>
+
+              {/* GitHub and publication links */}
               <div className="flex justify-center md:justify-start items-center space-x-3">
                 {project.github && <div className="py-3">
                   <a
@@ -81,7 +98,7 @@ export const Projects = () => {
               </div>
             </div>
 
-            {/* Image Section */}
+            {/* Project Screenshot */}
             <div className="flex-1 flex justify-center">
               <Image
                 className="border-1 dark:border-gray-700 shadow-lg rounded-sm"
