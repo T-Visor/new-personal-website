7460670105b9a49af2422ac9ea0606a7da2596a2
T-Visor <turkimbro@yahoo.com>
2025-07-31 07:31:47 -0400

    refactored FadeInView component to be more idiomatic

--- components/FadeInView.tsx
+++ components/FadeInView.tsx
@@ -1,23 +1,26 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
+import { ReactNode } from "react";
 
-const FadeInView = ({
-  children,
-  className,
-}: Readonly<{
-  children: React.ReactNode;
+interface FadeInViewProps {
+  children: ReactNode;
   className?: string;
-}>) => {
+}
+
+const FadeInView = ({ 
+  children, 
+  className 
+}: FadeInViewProps) => {
   return (
     <motion.div
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.8, ease: 'easeOut' }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
       viewport={{ once: true }}
       className={className}
     >
       {children}
     </motion.div>
-  )
-}
+  );
+};
 
 export default FadeInView;
